f3a6826835eafb121a91db070c46e45f12938525
Leo Laskin <llaskin@gmail.com>
2021-05-17 13:37:16 -0400

    adding items and description to room 7

--- rooms/room7.c
+++ rooms/room7.c
@@ -10,9 +10,12 @@ void create() {
 ::create();
     set_short("A dark room");
     set_day_long("The room has stalagtites and stalagmites "
-    "in the floor and ceilings.");
+    "in the floor and ceilings.  The ceiling is high above "
+    "you, and an iron sarcophagus sits in the north-east "
+    "corner of the room");
     set_items(([
     	({"stalactites", "stalagmites"}) : "Rocky outcroppings appear from the floor and ceiling",
+    	"sarcophagus": "This iron sarcophagus is sealed shut"
     ]));
     set_properties(([
 	"light" : SHIVA_LIGHT,
@@ -23,4 +26,4 @@ void create() {
 	"south" : SHIVA_ROOM + "room9.c",
     ]));
 }
-//The room has a high domed ceiling, and an iron sarcophagus sits in the north-east corner of the room
+//
