4a03a25918ace54ffa64d741de482c3acdfab043
Leo Laskin <llaskin@gmail.com>
2021-05-17 13:37:04 -0400

    adding items and description to room 5

--- rooms/room5.c
+++ rooms/room5.c
@@ -11,9 +11,14 @@ void create() {
 ::create();
     set_short("A dark room");
     set_day_long("The room has stalagtites and stalagmites "
-    "in the floor and ceilings.");
+    "in the floor and ceilings. Several alcoves are cut into"
+     " the east and west walls, and several pieces of "
+     "rotting wood are scattered throughout the room");
     set_items(([
     	({"stalactites", "stalagmites"}) : "Rocky outcroppings appear from the floor and ceiling",
+        "alcove" : "The alcove is cut into the wall",
+        "alcoves" : "Multiple alcovers are cut into each wall",
+         ({"wood", "rotting wood"}) : "These rotten pieces of wood seem useless",
     ]));
     set_properties(([
 	"light" : SHIVA_LIGHT,
