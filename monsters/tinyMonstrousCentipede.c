b25fb8b2339a9238f5a89cf36cf08a78cb54bdf6
Leo Laskin <llaskin@gmail.com>
2021-05-16 21:02:07 -0400

    adding monstrous centipede here.

---
+++ monsters/tinyMonstrousCentipede.c
@@ -0,0 +1,23 @@
+// Lord
+// Tiny Monstrous Centipede
+//https://dungeons.fandom.com/wiki/SRD:Tiny_Monstrous_Centipede
+
+ #include <std.h>
+ #include <shivaCaverns.h>
+
+inherit MONSTER;
+
+void create() {
+::create();
+    set_name("Monstrous Centipede");
+    set_short("Monstrous Centipede");
+    set_long("Monstrous centipedes tend to attack anything "
+    "that resembles food, biting with their jaws and injecting their poison.");
+    set_id(({"centipede", "monstrous centipede"}));
+    set_level(SHIVA_CHALLENGE_LEVEL1);
+    set_race("centipede");
+    set_body_type("insectoid");
+    set_gender("male");
+    //TODO: Make him have a poisonous bite
+    //TODO: Make Aggro till very high charisma
+}
