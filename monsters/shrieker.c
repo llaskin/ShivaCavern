748dd5003a3815d293e391a97267439611e91332
Leo Laskin <llaskin@gmail.com>
2021-05-16 21:32:36 -0400

    adding the shrieker mushroom!

---
+++ monsters/shrieker.c
@@ -0,0 +1,23 @@
+// Lord
+// shrieker
+//https://forgottenrealms.fandom.com/wiki/Shrieker
+
+ <std.h>
+ <shivaCaverns.h>
+
+inherit MONSTER;
+
+void create() {
+::create();
+    set_name("shrieker");
+    set_short("shrieker");
+    set_long("The shrieker is a variant of the violet "
+    "fungus known for the strange and loud noises it "
+    "emits to attract prey.");
+    set_id(({"shrieker"}));
+    set_level(SHIVA_BASELEVEL);
+    set_race("mushroom");
+    set_body_type("human");  //TODO: create race
+    set_gender("male");
+}
+//TODO: Give shriek ability?  Perhaps a short para?
