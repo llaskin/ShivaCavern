a9ad829a0a879d16644e1a230cd8a4a9f0556d1e
Leo Laskin <llaskin@gmail.com>
2021-05-18 15:27:59 -0400

    added next 10 rooms

---
+++ rooms/room70.c
@@ -0,0 +1,34 @@
+// by Lord
+// room, file=filename    
+
+anclude <std.h>
+anclude <shivaCaverns.h>
+
+inherit ROOM;
+
+void create() {
+::create();
+    set_short("short description");
+    set_long("night long description");
+    set_items(([
+	"item1" : "desc1",
+	"item2" : "desc2",
+	"item3" : "desc3",
+	({"item4", "item5", "item6"}):
+		"desc4",
+	({"item7", "item8"}) : "desc5",
+    ]));
+    set_properties(([
+	"light" : SHIVA_LIGHT,
+	"night light" : 0,
+    ]));
+    set_exits(([
+	"west" : SHIVAROOM + "room69",
+	"northeast" : SHIVAROOM + "room61",
+    ]));
+    }
+    void reset() {
+    if(!present("shrieker"))
+	new(SHIVA_MONSTERS + "shrieker")->move(this_object());
+
+}
