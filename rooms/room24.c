c64854a7d7fd745a70821741317ee19a6a7d8533
Leo Laskin <llaskin@gmail.com>
2022-08-21 19:08:54 -0400

    add poisoned spring

--- rooms/room24.c
+++ rooms/room24.c
@@ -10,9 +10,10 @@ void create() {
 ::create();
     set_short("A dark room");
     set_day_long("The room has stalactites and stalagmites "
-    "in the floor and ceilings.");
+    "in the floor and ceilings.  A spring of water bubbles from the south wall.");
     set_items(([
     	({"stalactites", "stalagmites"}) : "Rocky outcroppings appear from the floor and ceiling",
+    	"spring":"This water doesn't smell right."
     ]));
     set_properties(([
 	"light" : SHIVA_LIGHT,
@@ -22,4 +23,19 @@ void create() {
 	"north" : SHIVA_ROOM + "room10",
 	"northeast" : SHIVA_ROOM + "room11",
     ]));
+}
+void init() {
+::init();
+ add_action("drink", "drink");
+}
+
+int drink(string str) {
+  if(!str) return 0;
+  if(str!="water" && str!="spring") return 0;
+  message("info", "You bend down and drink some of the water trickling from "
+  "the wall.", this_player());
+  message("info", this_player()->query_cap_name()+" drinks some water from
+the spring.", this_object(), this_player());
+  if(this_player()->add_quenched(50)) this_player()->poison(10);
+  return 1;
 }
