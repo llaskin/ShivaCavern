36c4be1754a136c8e447347e8aacdcf2221b5320
Leo Laskin <llaskin@gmail.com>
2022-08-21 14:39:24 -0400

    fixing description of shield

--- armour/charredWoodenShield.c
+++ armour/charredWoodenShield.c
@@ -7,16 +7,14 @@ void create() {
 ::create();
     set_name("shield");
     set_short("a charred wooden shield");
-    set_long("The vest is made from a rather ridged leather, making "
-	"it appear as rather good protection.");
-    set_id(({"vest", "shield", "charred shield", "wooden shield", "charred wooden shield"}));
-    set_ac(3);            // Armour class of armour
+    set_long("The shield has burn marks.  It doesn't look like its worth using.");
+    set_id(({"shield", "charred shield", "wooden shield", "charred wooden shield"}));
+    set_ac(2);            // Armour class of armour
     set_mass(5);
-    set_value(150);
-    set_type("armour");   // Read help files for legal types
+    set_value(15);
+    set_type("shield");
     set_limbs(({"torso", "left arm"}));
-    set_material(({"leather"}));
-    set_size(9000);
+    set_material(({"wood"}));
 }
 //TODO: How to make this a shield, and make sure it doesn't
 //block other wearings
