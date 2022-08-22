6b31fbf177e3e24a14ceafc2e2b8f804c942c51a
Leo Laskin <llaskin@gmail.com>
2022-08-22 14:56:12 -0400

    setting darkmantle body and removing todo

--- rooms/room90.c
+++ rooms/room90.c
@@ -10,22 +10,22 @@ void create() {
 ::create();
     set_short("A dark room");
     set_day_long("The room has stalactites and stalagmites "
-    "in the floor and ceilings.");
+    "in the floor and ceilings.  There are scratches on the east wall");
     set_items(([
 	    ({"stalactites", "stalagmites"}) : "Rocky outcroppings appear from the floor and ceiling",
+	    "scratches":"These scratches look like they were created by a scared adventurer"
     ]));
     set_properties(([
-	"light" : SHIVA_LIGHT,
-	"night light" : 0,
+        "light" : SHIVA_LIGHT,
+        "night light" : 0,
     ]));
     set_exits(([
-	"north" : SHIVA_ROOM + "room85",
-	"southwest" : SHIVA_ROOM + "room95",
-    "southeast" : SHIVA_ROOM + "room96",
+        "north" : SHIVA_ROOM + "room85",
+        "southwest" : SHIVA_ROOM + "room95",
+        "southeast" : SHIVA_ROOM + "room96",
     ]));
-    }
-    void reset() {
+}
+void reset() {
     if(!present("shrieker"))
 	new(SHIVA_MONSTERS + "shrieker")->move(this_object());
-
 }//TODO
